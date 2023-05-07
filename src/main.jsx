6d556790b53f9e12ba3564335428f56b5c5a04f1
Will-Swinson <william.swinson01@gmail.com>
2023-05-07 15:17:21 -0500

    JUST ADDED THAT PACKAGE JSON

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import "./index.css";
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
-if (ProcessingInstruction.env.NODE_ENV === "production") disableReactDevTools;
+// if (ProcessingInstruction.env.NODE_ENV === "production") disableReactDevTools;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
